0f92fe9e9e31fbed097481fcb5d438f43d798222
kuldeepsingh18 <you@example.com>
2023-08-21 14:38:39 +0530

    chang

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,17 +12,16 @@ const Footer = () => {
                 </div>
                 <p data-aos="zoom-in-left" data-aos-duration="1500" className='m-0 ff_lato fs_lg fw_semibold cl_F1AC73 text-center  mt-2'>Cyber Drops</p>
                 <p data-aos="zoom-in-left" data-aos-duration="1500" className='m-0 ff_lato fs_xsm fw-normal cl_fff m-auto vivera mt-3 text-center'>Viverra metus viverra rhoncus eget quam suspendisse diam amet, nisl. Mattis maecenas libero lacinia accumsan amet.</p>
-             
-                    <div data-aos="zoom-in-left"     className='d-flex justify-content-center mt-4 gap-3'>
-                        <div className='nav_cercle d-flex justify-content-center align-items-center cp'><Ship /></div>
-                        <div className='nav_cercle d-flex justify-content-center align-items-center cp'><Discord /></div>
-                        <div className='nav_cercle d-flex justify-content-center align-items-center cp'><Twiter /></div>
-                        <div className='nav_cercle d-flex justify-content-center align-items-center cp'><M_logo /></div>
-                    </div>
-           
+
+                <div data-aos="zoom-in-left" className='d-flex justify-content-center mt-4 gap-3'>
+                    <div className='nav_cercle d-flex justify-content-center align-items-center cp'><Ship /></div>
+                    <div className='nav_cercle d-flex justify-content-center align-items-center cp'><Discord /></div>
+                    <div className='nav_cercle d-flex justify-content-center align-items-center cp'><Twiter /></div>
+                    <div className='nav_cercle d-flex justify-content-center align-items-center cp'><M_logo /></div>
+                </div>
             </div>
-        <hr className='cl_fff'></hr>
-        <p className='m-0 ff_lato fs_xsm fw-normal cl_fff text-center opacity-50'>Copyright By 2022</p>
+            <hr className='cl_fff'></hr>
+            <p className='m-0 ff_lato fs_xsm fw-normal cl_fff text-center opacity-50'>Copyright By 2022</p>
         </div>
     )
 }
